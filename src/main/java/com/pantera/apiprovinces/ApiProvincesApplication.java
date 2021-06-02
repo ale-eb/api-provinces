a4a107c57f89f05c9b05302d0f146d8d61b91552
Ale <aleee.eb@gmail.com>
2021-06-02 09:22:22 -0300

    Initial commit

---
+++ src/main/java/com/pantera/apiprovinces/ApiProvincesApplication.java
@@ -0,0 +1,13 @@
+package com.pantera.apiprovinces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProvincesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProvincesApplication.class, args);
+	}
+
+}
